fe533ceff318d0f95bba4782d6e2e2df4f24543f
Manjunath0408 <nath.vasam@gmail.com>
2022-04-03 15:00:19 +0530

    cards, nav, login - done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
+import Login from "./Containers/Login";
+
 function App() {
-  return (
-    <div class="text-red-400">
-      Hello World
-    </div>
-  );
+    return (
+        <>
+            {/* // <Login /> */}
+            <Navbar />
+            <Card />
+            <Card />
+            <Card />            
+        </>
+    );
 }
 
 export default App;
