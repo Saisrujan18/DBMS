1b6104d5687d1e7efac7d5fd17a769cbc23d2277
Manjunath0408 <nath.vasam@gmail.com>
2022-04-14 17:06:42 +0530

    Cart, Home, Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React,{useState} from "react";
 import Card from "./Components/Card";
 import Navbar from "./Components/Navbar";
 import Home from "./Containers/Home";
 import Login from "./Containers/Login";
+import Cart from "./Containers/Cart";
 import UserProfile from "./Containers/UserProfile";
 import SellerPage from "./Containers/SellerPage";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Routes,
+} from "react-router-dom";
 
 function App() {
-    return (
-        <div className="bg-gray-100">
-            <SellerPage/>
-        </div>
-    );
+    const [cartOpen, setCartOpen] = useState(false);
+	return (
+		<Router>
+			<Navbar open={cartOpen} setOpen={setCartOpen}/>
+            <Cart open={cartOpen} setOpen={setCartOpen}/>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
